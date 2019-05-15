41b92bf6db24182d55c5731ce9575fb1d5bf661d
NEM907 <nathanseaw@gmail.com>
2019-05-15 09:51:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-
+function initialize(){
+  
+  
+  var int;
+  
+  if(int == true) {
+    return console.log('initialized fail');
+  } else if(int == false) {
+    return console.log('initialized success');
+  }
+  
+}
